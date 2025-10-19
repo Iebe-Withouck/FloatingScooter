ecf46634ccc48e3e8aa315b1302e89f9e174ae6e
Iebe Withouck <iebe.withouck@student.howest.be>
2025-10-19 20:53:29 +0200

    changes

--- js/style.js
+++ js/style.js
@@ -1,6 +1,3 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.161.0/build/three.module.js';
-import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.161.0/examples/jsm/loaders/GLTFLoader.js';
-
 const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 scene.background = new THREE.Color(0x000010)
