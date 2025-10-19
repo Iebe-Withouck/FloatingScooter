4ee851017b4d53478e99beae7a9342e80e4f1fa9
Iebe Withouck <iebe.withouck@student.howest.be>
2025-10-19 21:15:37 +0200

    m

--- js/style.js
+++ js/style.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three'
+import * as THREE from './three'
 import { GLTFLoader } from './three/examples/jsm/loaders/GLTFLoader.js'
 
 const canvas = document.querySelector('canvas.webgl')
